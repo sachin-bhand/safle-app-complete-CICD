347a00b1e922f920d476fb0d550fa8bb22afe391
sachin-bhand <sachinbhand06@gmail.com>
2026-04-23 15:34:31 +0530

    chnages pushed

---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+output "ecr_repo_url" {
+  value = aws_ecr_repository.safle-ecr-repo.repository_url
+}   
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.safle-cluster.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.safle-service.name
+}
